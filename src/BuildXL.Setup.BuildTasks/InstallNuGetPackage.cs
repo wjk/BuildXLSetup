96819fa754aed2fb14da28a452dfaf5a0823aec3
William Kent <wjk@users.noreply.github.com>
2019-08-06 18:46:46 -0400

    Add BuildXL.Setup.BuildTasks project

---
+++ src/BuildXL.Setup.BuildTasks/InstallNuGetPackage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace BuildXL.Setup.BuildTasks
+{
+    public class InstallNuGetPackage : ToolTask
+    {
+        [Required]
+        public ITaskItem OutputDirectory { get; set; }
+        [Required]
+        public string Feed { get; set; }
+        [Required]
+        public string PackageName { get; set; }
+        [Required]
+        public string PackageVersion { get; set; }
+
+        protected override string ToolName => "NuGet.exe";
+
+        protected override string GenerateFullPathToTool()
+        {
+            string location = typeof(InstallNuGetPackage).Assembly.Location;
+            return Path.GetDirectoryName(location);
+        }
+
+        protected override string GenerateCommandLineCommands()
+        {
+            List<string> argv = new List<string>();
+
+            argv.Add("install");
+            argv.Add("-OutputDirectory");
+            argv.Add(OutputDirectory.ItemSpec);
+            argv.Add("-Source");
+            argv.Add(Feed);
+            argv.Add(PackageName);
+            argv.Add("-Version");
+            argv.Add(PackageVersion);
+
+            return string.Join(" ", argv.Select(arg =>
+            {
+                if (arg.Contains(" ")) return $"\"{arg}\"";
+                else return arg;
+            }));
+        }
+    }
+}
