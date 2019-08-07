7091c51ee6adb779f7249ce9065402f815717773
William Kent <wjk@users.noreply.github.com>
2019-08-06 20:56:40 -0400

    Add /edit action to ShellExtension project

--- src/BuildXL.ShellExtension/Program.cs
+++ src/BuildXL.ShellExtension/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -14,8 +15,27 @@ namespace BuildXL.ShellExtension
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            MessageBox.Show(StringResources.DoNotRunDirectlyDialog_Instruction, StringResources.WindowTitle,
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            if (args.Length == 2 && args[0].Equals("/edit", StringComparison.OrdinalIgnoreCase))
+            {
+                string path = args[1];
+                if (File.Exists(path))
+                {
+                    var startInfo = new ProcessStartInfo(Path.Combine(Environment.SystemDirectory, "notepad.exe"));
+                    startInfo.Arguments = $"\"{path}\"";
+                    startInfo.UseShellExecute = true;
+                    Process.Start(startInfo);
+                }
+                else
+                {
+                    string message = string.Format(StringResources.FileNotFoundDialog_Instruction, path);
+                    MessageBox.Show(message, StringResources.WindowTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show(StringResources.DoNotRunDirectlyDialog_Instruction, StringResources.WindowTitle,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
