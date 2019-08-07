fb70f7023561ac867db984d6086136acf6d77f5c
William Kent <wjk@users.noreply.github.com>
2019-08-06 20:36:22 -0400

    Add BuildXl.ShellExtension project

---
+++ src/BuildXL.ShellExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BuildXL.ShellExtension
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            MessageBox.Show(StringResources.DoNotRunDirectlyDialog_Instruction, StringResources.DoNotRunDirectlyDialog_WindowTitle,
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
